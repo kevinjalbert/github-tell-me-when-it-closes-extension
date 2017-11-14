8042961dd589792ca96a95737e44f1cbf7e4538c
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2017-11-14 13:26:59 -0500

    Bump to V0.2.1 - fix duplicate button bug, simplify double page tracking
    An issue could happen when using the browser's history navigation
    that'll results in duplicate buttons being created on the page. To
    counter this we simply label the button's Id and check for it before
    adding a new one.
    
    Added some comments and formatting fixes.

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,26 +1,21 @@
 import ext from "./utils/ext";
 
-const doublePageTracker = {};
-
+// For when page transitions occur
 ext.webNavigation.onHistoryStateUpdated.addListener(
    function (event) {
-      if(doublePageTracker[event.url]) {
-        ext.tabs.executeScript(event.tabId, {
-          file: "scripts/contentscript.js"
-        });
-        delete doublePageTracker[event.url]
-      } else {
-        doublePageTracker[event.url] = true;
-      }
+      ext.tabs.executeScript(event.tabId, {
+        file: "scripts/contentscript.js"
+      });
    },
-   { url:   [  {urlMatches: 'github\.com\/.*?\/.*?\/(issues|pull)'}   ] }
+   { url: [{urlMatches: 'github\.com\/.*?\/.*?\/(issues|pull)'}] }
 );
 
+// For when the page first loads (refresh)
 ext.webNavigation.onCompleted.addListener(
    function (event) {
       ext.tabs.executeScript(event.tabId, {
         file: "scripts/contentscript.js"
       });
    },
-   { url:   [  {urlMatches: 'github\.com\/.*?\/.*?\/(issues|pull)'}   ] }
+   { url: [{urlMatches: 'github\.com\/.*?\/.*?\/(issues|pull)'}] }
 );
--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,25 +1,34 @@
 import ext from "./utils/ext";
 
-ext.runtime.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
+const ID = "github-tellmewhenitcloses-button"
 
+ext.runtime.sendMessage({}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+
+      const button = document.getElementById(ID)
+
+      // Exit early if button already exists
+      if (button !== null) { return }
+
+      // Grab notification element on page
       const threadSubscriptionStatus = document.getElementsByClassName("thread-subscription-status")[0];
 
-			const openLink = function() {
+      const openLink = function() {
         window.location = "https://tellmewhenitcloses.com?url=" + location.href;
-			};
+      };
 
-			const tellMeWhenButton = document.createElement("button");
+      const tellMeWhenButton = document.createElement("button");
       tellMeWhenButton.className = "btn btn-sm";
+      tellMeWhenButton.id = ID;
       tellMeWhenButton.style.width = '100%';
       tellMeWhenButton.style.marginBottom = '10px';
-			tellMeWhenButton.onclick = openLink;
-			tellMeWhenButton.innerText = "Tell Me When It Closes";
+      tellMeWhenButton.onclick = openLink;
+      tellMeWhenButton.innerText = "Tell Me When It Closes";
 
+      // Stick button in-front of all child elements of notifications
       threadSubscriptionStatus.parentNode.insertBefore(tellMeWhenButton, threadSubscriptionStatus.previousSibling);
-
-		}
-	}, 10);
+    }
+  }, 10);
 });
